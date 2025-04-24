9c5512891450b4b99f493855900367b865494e51
Jelle Maas <typiqally@gmail.com>
2025-04-24 23:03:58 +0200

    Use copy paste in favour of CSV file

--- quickstart.py
+++ quickstart.py
@@ -3,12 +3,21 @@ import json
 import os
 
 
-def read_accounts(file_path, dialect='excel', delimiter='\t'):
-    with open(file_path) as csv_file:
-        reader = csv.DictReader(csv_file, dialect=dialect, delimiter=delimiter)
-        rows = [row for row in reader]
+def input_accounts(columns, delimiter='\t'):
+    lines = []
 
-    return rows
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+
+        if not line:
+            break
+
+        lines.append(line)
+
+    return [dict(zip(columns, line.split('\t'))) for line in lines]
 
 
 def arguments_to_string(args_dict):
@@ -23,14 +32,17 @@ def generate_quickstart(account: dict):
         "flags": ["-covert", "-fresh"],
         "layout": "resizable_modern",
         "fps": "10",
-        "script": f"{account['Script']}",
-        "breaks": f"{account['Breaks']}"
+        "script": f"{account['Script']}"
     }
 
-    if account["Params"] != '':
+    if account.get("Params", None) is not None:
         quick_start["params"] = [account['Params']]
 
+    if account.get("Breaks", None) is not None:
+        quick_start["breaks"] = f"{account['Breaks']}"
+
     return quick_start
+
 
 template = """
 @echo off
@@ -42,7 +54,16 @@ start "" /b javaw -Xmx{allocate_ram} -jar "%DREAMBOT_JAR%" -json "{quick_start_f
 cwd = os.getcwd()
 print(f"Current working directory: {cwd}")
 
-accounts = read_accounts('account_details.csv')
+print("Enter/Paste your content. Ctrl-D or Ctrl-Z ( windows ) to save it.")
+accounts = input_accounts(
+    [
+        "System", "Method", "Status",
+        "Banned", "Username", "E-mail",
+        "Password", "TOTP secret", "Proxy",
+        "Previous Proxy", "Script", "Params",
+        "Breaks"
+    ]
+)
 
 for index, account in enumerate(accounts):
     print(account)
